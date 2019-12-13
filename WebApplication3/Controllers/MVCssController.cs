b545755373c450628628f160d9ec2bacfac09abd
nghiemhuuhoang97@outlook.com <nghiemhuuhoang97@gmail.com>
2019-12-13 19:13:22 +0700

    Add project files.

---
+++ WebApplication3/Controllers/MVCssController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication3.data;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class MVCssController : Controller
+    {
+        private readonly Db _db;
+        public MVCssController(Db db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View(_db.MVCs.ToList());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(MVC mVC)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(mVC);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(mVC);
+        }
+        
+    }
+}
---
+++ WebApplication3/data/Db.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication3.Models;
+
+namespace WebApplication3.data
+{
+    public class Db:IdentityDbContext
+    {
+        public Db(DbContextOptions<Db> options):base(options)
+        {
+
+        }
+        public DbSet<MVC> MVCs { get; set; }
+    }
+}
